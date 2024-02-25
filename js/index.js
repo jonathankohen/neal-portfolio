05d0aefb3787c7e54e4af8d65534890caeebf47a
Jonathan Kohen <jonathan.a.kohen@gmail.com>
2024-02-25 03:00:18 -0500

    Finished contact section. Almost.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-var tagline = document.getElementById('tagline');
+const tagline = document.getElementById('tagline');
 
-var typewriter = new Typewriter(tagline, {
+const typewriter = new Typewriter(tagline, {
 	loop: true,
 });
 
@@ -14,3 +14,11 @@ typewriter
 	.typeString('A Visual Storyteller.')
 	.pauseFor(2500)
 	.start();
+
+const sendEmail = () => {
+	const email = document.getElementById('email').value;
+	const message = document.getElementById('message').value;
+	window.open(
+		`mailto:${email}?subject=Design%20Project%20Inquiry&body=${message}`,
+	);
+};
